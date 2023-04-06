e4701c9135639b5c17fbc5ea3c1222b5112b5fe7
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-04-06 13:57:27 +0500

    Mobile video

--- src/components/Main/bgvideo.jsx
+++ src/components/Main/bgvideo.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Video from "./../../assets/intro.mp4";
+import Video2 from "./../../assets/in.mp4";
 const bgvideo = () => {
   return (
     <>
-      <video src={Video} className="-z-10 bg-video" loop muted autoPlay></video>
+      <video src={Video} className="-z-10 desktop bg-video" loop muted autoPlay></video>
+      <video src={Video2} className="-z-10 bg-video mobile" loop muted autoPlay></video>
     </>
   );
 };
