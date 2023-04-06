fcd265956a27724e1dc2173596ccbaa04bc1f7d5
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-04-06 13:53:10 +0500

    Ok

--- src/components/Header/Hamburger.jsx
+++ src/components/Header/Hamburger.jsx
@@ -10,7 +10,7 @@ const Hamburger = () => {
   } = customHook();
   return (
     <>
-      <div className="header__menu relative flex items-center justify-between gap-5">
+      <div className="header__menu flex items-center justify-between gap-5">
         <div className="header__start text-white font-bold text-xl" id="start">
           КАК НАЧАТЬ
         </div>
@@ -26,24 +26,24 @@ const Hamburger = () => {
               className="w-9 h-9 icon_menu text-red-600"
             />
           )}
+          <ul
+            className={`open__menu  absolute items-end justify-end flex-col bg-white list-none w-48 p-4 rounded-md border ${
+              open ? "flex" : "hidden"
+            }`}
+          >
+            <li>О компании</li>
+            <li className="border-t-2 border-red-600 w-full text-end">
+              Продукция
+            </li>
+            <li>Каталог</li>
+            <li>PDF-каталог</li>
+            <li>Сертификаты</li>
+            <li>Новости</li>
+            <li className="border-t-2 border-red-600 w-full text-end">
+              Контакты
+            </li>
+          </ul>
         </div>
-        <ul
-          className={`open__menu  absolute items-end justify-end flex-col top-8 -left-38 bg-white list-none w-48 p-4 rounded-md border ${
-            open ? "flex" : "hidden"
-          }`}
-        >
-          <li>О компании</li>
-          <li className="border-t-2 border-red-600 w-full text-end">
-            Продукция
-          </li>
-          <li>Каталог</li>
-          <li>PDF-каталог</li>
-          <li>Сертификаты</li>
-          <li>Новости</li>
-          <li className="border-t-2 border-red-600 w-full text-end">
-            Контакты
-          </li>
-        </ul>
       </div>
     </>
   );
